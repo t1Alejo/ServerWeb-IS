ccb294b7509c816e66fa2763e83c316203b7a99c
Alejo <110114748+t1Alejo@users.noreply.github.com>
2026-09-02 23:56:03 -0300

    DELETE y PATCH implementados
    Correccion real de tasks y añadido de ID. Implementacion de DELETE y PATCH

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import json
 from wsgiref.simple_server import make_server
 
-tasks = [{"id": 1, "titulo": "prueba", "estado": False}]
+tasks = []
+ID = 0
 
 def app(environ, start_response):
     verb = environ.get("REQUEST_METHOD")
@@ -48,9 +49,63 @@ def app(environ, start_response):
         status = "201 Created"
         response_body = json.dumps(nuevatask).encode("utf-8")
         headers = [("Content-Type", "application/json"),("Content-length", str(len(response_body)))]
-
         start_response(status, headers)
         return [response_body]
+        
+    elif verb == "DELETE":
+        if len(path) == 3:
+            index = path[2]
+            index = int(index)
+            encuentra = 0
+            for i in range (len(tasks)):
+                if(tasks[i]["id"] == index):
+                    del(tasks[i])
+                    encuentra = 1
+                    break
+            if encuentra == 1:
+                status = "200 OK"
+                start_response(status, headers)
+                return [b""]
+            else:
+                status = "404 Not Found"
+                start_response(status, headers)
+                return [b"Not Found"]
+
+    elif verb == "PATCH":
+        if len(path) == 3:
+            index = path[2]
+            index = int(index)
+            item_encon = None
+            encuentra = 0
+            for item in tasks:
+                if item["id"] == index:
+                    item_encon = item
+                    encuentra = 1
+                    break
+
+            if encuentra == 1:
+                content_length = int(environ.get("CONTENT_LENGTH", 0))
+                body = environ ["wsgi.input"].read(content_length)
+
+                data = json.loads(body)
+
+                if "title" in data:
+                    item_encon["title"] = data["title"]
+                if "done" in data:
+                    item_encon["done"] = data["done"]
+
+                status = "200 OK"
+                start_response(status, headers)
+                return [json.dumps(item_encon).encode("utf-8")]
+            else:
+                status = "404 Not Found"
+                start_response(status, headers)
+                return [b"Not Found"]
+
+    if len(path) >= 2 and path[1] == "tasks":
+        status = "405 Method Not Allowed"
+        start_response(status, headers)
+        return[b""]
         
 with make_server('', 9292, app) as httpd:
     print("Serving on port 9292...")
